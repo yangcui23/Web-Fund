58f0a28362a1084c3ecf662ea1276130848a424b
Yang Cui <123989119+yangcui23@users.noreply.github.com>
2023-03-17 16:36:16 -0600

    Add files via upload

---
+++ Likes/script.js
@@ -0,0 +1,17 @@
+
+var like =  9 ;
+function change() {
+    like++;
+    document.querySelector("#like").innerText= like + " like(s)";
+}
+var like1 =  12 ;
+function change1() {
+    like1++;
+    document.querySelector("#like1").innerText= like1 + " like(s)";
+}
+var like2 =  9 ;
+
+function change2() {
+    like2++;
+    document.querySelector("#like2").innerText= like2 + " like(s)";
+}
